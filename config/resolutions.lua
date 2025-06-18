8f9806e1d4f0848d11be2174d5ecba44b7793bf1
typedin <antoine@typedin.dev>
2025-06-18 16:07:29 +0200

    a bunch of predefined resolutions

--- config/resolutions.lua
+++ config/resolutions.lua
@@ -1,6 +1,37 @@
-return {
-    {
-        width = 1060,
+local resolutions = {
+    default = {
+        width = 1280,
         height = 800,
     },
+    -- aspect ratio 8:5
+    atari_st = {
+        width = 320,
+        height = 200,
+    },
+    c64 = {
+        -- aspect ratio : 8:5
+        width = 320,
+        height = 200,
+    },
+    -- aspect ratio 8:5
+    amiga = {
+        width = 320,
+        height = 200,
+    },
+    -- aspect ratio 8:5
+    vga_dos = {
+        width = 320,
+        height = 200,
+    },
+    -- aspect ratio : 16:15
+    nes = {
+        width = 256,
+        height = 240,
+    },
+    snes = {
+        width = 256,
+        height = 224,
+    },
 }
+
+return resolutions
